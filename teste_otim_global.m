bd303322631db1b980375b84aee7b9393b4577d3
martinsavi <114028902+martinsavi@users.noreply.github.com>
2023-10-25 23:14:16 -0300

    otimização global adicionada

--- teste_otim_global.m
+++ teste_otim_global.m
@@ -1,8 +1,9 @@
 clear; clc;
-a0 = [0.50;0.45;0.32;0.205;1e-5;6.5]; 
-lb = [0.40,0.30,0.30,0.100,0   ,5.5  ];
-ub = [0.60,0.60,0.50,0.300,5e-1,6.5];
+a0 = [0.5592,    0.4500,    0.3565,    0.1560,    4e-5,    4.2130];
+lb = [0.40,0.30,0.20,0.150,4e-5   ,4.2];
+ub = [0.80,0.45,0.50,0.350,5e-3,4.4  ];
 
+melhor_ate_agora=[0.5592,    0.4500,    0.3565,    0.1560,    4e-5,    4.2130];
 %   Iexp = [1.03759418890913	1.93984923218668	4.2105267859144	5.50375960737219	6.48120319236476	7.39849617344079	8.25563855060027	10.2857136886617	11.0676692450222	12.0902260697715
 % ]';
 %     Vexp = [0.66286288281833	0.612695560680013	0.51870282474702	0.517403067839343	0.48963963129269	0.462366184933421	0.423861324718456	0.326127045742691	0.279645383820309	0.229047044443503
@@ -15,14 +16,17 @@ ub = [0.60,0.60,0.50,0.300,5e-1,6.5];
 
     v0 = [1.25 1.20 1.15 1.0 0.9 0.8 0.7 0.66 8.4 8.1 7.5 7.0 0.05 -0.2 0.4 6.0]'; %chute ini
     
+% options = optimoptions('ga','TimeLimit',60);
+% [x,fval] = ga(@(a) obj(a,Iexp,Vexp,v0),size(a0,1),[],[],[],[],lb,ub,[],options);
 
-[x,fval] =  fmincon(@(a) obj(a,Iexp,Vexp,v0),a0,[],[],[],[],lb,ub);
-Vteo = linspace(0.05,Vexp(1),50);
+options = optimoptions('simulannealbnd','TimeLimit',60*2);
+[x,fval] = simulannealbnd(@(a) obj(a,Iexp,Vexp,v0),a0,lb,ub);
+Vteo = linspace(0.05,Vexp(1),30);
 
 for i = 1:size(Vteo,2)
         options = optimset('Display','off');
         [sol] = fsolve(@(v) sistemaNL(x,v,Vteo(i)),v0,options);
-        Iteo(i) = sol(16);
+        Iteo(i) = real(sol(16));
 end
 
 plot(Iexp,Vexp,'o',Iteo,Vteo)
@@ -39,7 +43,7 @@ function o = obj(a,Iexp,Vexp,v0)
         csvwrite('sols.csv',sol)
         Iteo(i) = sol(16);
     end
-    c = [Iteo+0.01; a+0.01]
+    c = [a+0.00001]
 %       o = (1-a(1))^2 +100*(a(2)-a(1)^2)^2; Rosenbrock funciona ok
       o = sum((Iteo-Iexp).^2); %MMQ
       
@@ -90,56 +94,12 @@ function fv = sistemaNL(a,v,vcell)
     fv(12) = 3600*v(16)/4/F - D*(v(11)- v(12))/espce;
     % fv(13) = ((V*Kdec*v(13))/(A*YxA)) + ((q)/(A*fx*YxA)*(M - v(13))); % equação 4 do artigo igualada a zero
     fv(13) = kk*exp((alfaa*v(14)*F)/R/T)*v(2)/(K(1)+v(2))*v(13)-((V*Kdec*v(13))/(A*YxA)) + ((q)/(A*fx*YxA)*(M - v(13)));
-    fv(14) = -vcell + Ecell -v(15)- v(16)*Rcell - v(14)-R*T/F*log(ilim/(ilim-v(16))); % primeira equação do frame 3
+    fv(14) = -vcell + Ecell -v(15)- v(16)*Rcell - v(14)-R*T/F*log(ilim/sqrt((ilim-v(16))^2)); % primeira equação do frame 3
     fv(15) = 3600*v(16)/20/F - kk*exp((alfaa*v(14)*F)/R/T)*v(2)/(K(1)+v(2))*v(13); % terceira equação do frame 3
     fv(16) = v(16)-(Iref*(((v(11)+v(12))/2)/Cref)*exp(alfac*v(15)*F/R/T)); 
 end
 
 
-function [c,ceq] = cons(a)
 
-    T = 303; %K
-    R = 8.314; % m³.atm/K.mol
-    P = 101325; %atm
-    oxi0 = (0.21*P)/(R*T); % valor de entrada - mol/m³
-
-    %D = 0.86^2.5*((T^1.75*5.8/10000)/(27.772*P))*3600/10000;
-    D=0.08*10^-4*3600;
-
-    Iref=0.0064;
-    Cref=1;
-
-    v = [1.25 1.20 1.15 1.0 0.9 0.8 0.7 0.66 8.4 8.1 7.5 7.0 0.05 -0.2 0.4 6.0]'; %chute ini
-    v = csvread('sols.csv');
-
-    v0 = 1.56 ;% mol/m³ - concentração inicial
-    fx = 10 ;% assumido pelo artigo
-    kk = 0.207 ;% mol/m³.h 
-    K = [0.592 0.8 0.8 0.8]'; % os 3 últimos valores foram assumidos pelo artigo (K2-4)
-    alfaa = a(1); %0.5 % constante
-    alfac = a(2); %0.44 % constante
-    Kdec = 8.33*10^-4; % valor assumido pelo artigo
-    q = 2.25*10^-5;  % vazão volumétrica - m³/h
-    V = 1.596*10^-5; % m³ - volume
-    F = 96485; % Constante de faraday - C/mol
-    A = 1.2*10^-3; % área - m²
-    YxA = 0.05; % rendimento 
-    Ecell = 0.32; % volts
-    Rcell = 4.9389*10^-5; % m³/S
-    M=0.05; %quantidade de microorganismos no início (segundo artigo,0) mol/m³
-    espce=0.000023; % assumido pelo artigo - m
-
-    c=zeros(1,8);
-    c(1) = v(1)- v0;
-    c(2) = kk*exp((alfaa*v(14)*F)/R/T)*v(2)/(K(1)+v(2))*v(13)-(q/A)*(v(1)-v(2)); % Equação 3-13
-    c(3) = v(3)- v(2);
-    c(4)= v(4)- K(2)*v(3);
-    c(5) = v(5)- v(4);
-    c(6) = v(6)- K(3)*v(5);
-    c(7) = v(7)- v(6);
-    c(8) = v(8);
-
-    ceq=[];
-end
 
 
